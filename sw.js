f6194f2c7925d13ab5b50f7944cd99683149da89
mesut0756 <mesutabdi25@gmail.com>
2026-03-12 20:54:45 +0300

    pop

---
+++ sw.js
@@ -0,0 +1,11 @@
+
+(function(mulkcsa){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = mulkcsa || {};
+s.src = "\/\/plumprush.com\/c.D\/9\/6ebI2t5\/lvSyWVQ\/9qNrjjgI2KMvjqkj0UMXSY0c2cOFDrY\/y\/OjT\/QP1i";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
